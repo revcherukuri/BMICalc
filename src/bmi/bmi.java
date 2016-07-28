4b27dcd265a85de3da0279e5339f40aac6de4e7b
rev.cherukuri@gmail.com <rev.cherukuri@gmail.com>
2016-07-28 14:44:47 -0700

    Improved user interaction to accomodate all bmis

--- src/bmi/bmi.java
+++ src/bmi/bmi.java
@@ -1,7 +1,7 @@
 package bmi;
 import java.util.Scanner;
 public class bmi {
- 
+ static double bmi = 0.0;
  public static void main(String[]args)
  {
   printOutput();
@@ -10,18 +10,18 @@ public class bmi {
  public static void printOutput()
  {
   Scanner read = new Scanner(System.in);
-  System.out.println("Before we begin, would you prefer entering your height and weight in kg/meters or lb/feet?");
-  String answer = read.next();
-  if(answer.equals("kg/meters")){
+  System.out.println("Before we begin, would you prefer entering your height and weight in (kg/meters - 1) or (lb/feet - 2)?");
+  int answer = read.nextInt();
+  if(answer == 1){
   System.out.println("Please enter your height in meters");
   double height = read.nextDouble();
    System.out.println("Please enter your weight in kilos");
    double weight = read.nextDouble();
-   double bmi = weight/(height*height);
+   bmi = weight/(height*height);
    System.out.println("Your body mass index is " + bmi + " kg/m^2");
   
   }
-  else if(answer.equals("lb/feet")){
+  else if(answer == 2){
    System.out.println("Please enter your height in feet");
    double feet = (read.nextDouble())*12;
    System.out.println("Please enter your remaining inches");
@@ -29,50 +29,52 @@ public class bmi {
    double height1 = feet + inches;
    System.out.println("Please enter your weight in pounds");
    double weight1 = (read.nextDouble());
-   double bodymassindex = (weight1/(height1*height1))*703;
-   System.out.println("Your body mass index is " + bodymassindex + " lb/in^2");
+   bmi = (weight1/(height1*height1))*703;
+   System.out.println("Your body mass index is " + bmi + " lb/in^2");
    
   }
   
   
-  System.out.println("Let's see if you are healthy");
-  //System.out.println("What is your height? Please write feet'inches''");
-  //String finalheight = read.next();
-  //if(finalheight.equals("4'10''"))
-  //{
-	  System.out.println("What was your bmi? (2 decimal places)");
-	  double finalbmi = read.nextDouble();
-	  if(finalbmi <= 24 && finalbmi >= 19)
+  System.out.println("Let's see if you are healthy"); 
+	  if(bmi <= 24 && bmi >= 19)
 	  {
-		  if(finalbmi >= 22)
+		  if(bmi >= 22)
 		  {
 			  System.out.println("You are in the healthy range, but you are nearing the overweight range");
 		  }
-		  else if(finalbmi < 22){
+		  else if(bmi < 22){
 			  System.out.println("You are in the healthy range");
 		  }
 	  }
-	  else if(finalbmi >= 25 && finalbmi <= 29)
+	  else if(bmi >= 25 && bmi <= 29)
 	  {
-		  if(finalbmi > 27)
+		  if(bmi > 27)
 		  {
 			  System.out.println("You are overweight, and are nearing the obese category");
 		  }
-		  else if(finalbmi <= 27)
+		  else if(bmi <= 27)
 		  {
 			  System.out.println("You are just a bit overweight");
 		  }
 	  }
-	  else if(finalbmi >= 30 && finalbmi <= 35)
+	  else if(bmi >= 30 && bmi <= 35)
 	  {
-		  if(finalbmi >= 33)
+		  if(bmi >= 33)
 		  {
 			  System.out.println("Watch your health, you are very obese");
 		  }
-		  else if(finalbmi < 33)
+		  else if(bmi < 33)
 		  {
 			  System.out.println("You are fairly obese");
 		  }
+	  }
+	  else if(bmi < 19)
+	  {
+		  System.out.println("You are a bit underweight");
+	  }
+	  else if(bmi > 35)
+	  {
+		  System.out.println("You're obesity is a danger to your health");
 	  }
   }
   
